9645698f06557b146f7ab1733b469d945a988908
yugandhara <yuga.dange@gmail.com>
2024-12-01 22:13:55 +0530

    new update

--- doctype/shop_info/shop_info.py
+++ doctype/shop_info/shop_info.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, yugandhara and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
